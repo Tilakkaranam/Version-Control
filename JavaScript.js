3182c0465e7c1206f450c9833619ab487bf688f2
Karanam Tilak <59948044+Tilakkaranam@users.noreply.github.com>
2020-08-31 21:13:35 +0530

    Create JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,2 @@
+const myHeading = document.querySelector('p');
+myHeading.textContent = 'JavaScript ("JS" for short) is a full-fledged dynamic programming language that can add interactivity to a website.It was invented by Brendan Eich (co-founder of the Mozilla project), the Mozilla Foundation, and the Mozilla Corporation.JavaScript is versatile and beginner-friendly.';
